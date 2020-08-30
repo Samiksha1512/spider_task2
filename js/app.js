385b43c94830b62e0d86cf5b1d4cce34e27848fa
Samiksha1512 <sheksami15@gmail.com>
2020-08-30 17:13:41 +0530

    questions and script

---
+++ js/app.js
@@ -0,0 +1,131 @@
+const quenmb = document.querySelector(".question-nmb");
+const queText = document.querySelector(".question-text");
+const optcont = document.querySelector(".opt-cont");
+const homeBox = document.querySelector(".home-box");
+const quizBox = document.querySelector(".quiz-box");
+const resultBox = document.querySelector(".result-box");
+let index = 0;
+let quesCounter = 0;
+let currentques;
+let availablequestions = myQuestions;
+let availableoptions = [];
+let correctans = 0;
+let attempt=0;
+
+
+
+function getNewQues(n){
+	if(n==0 || n>myQuestions.length){
+	document.getElementById('prevbtn').style.display="none";
+}
+else{
+	document.getElementById('prevbtn').style.display="block";
+}
+
+console.log(n);
+	quenmb.innerHTML = "Question "+(quesCounter+1)+" of " + myQuestions.length;
+	const queindex = availablequestions[n];
+	queText.innerHTML = queindex.question;
+	console.log(availablequestions[n]);
+	//console.log(availablequestions[n].options);
+	let optlen = queindex.options.length;
+	//console.log(optlen);
+	let animationdelay = 0.15;
+	for(let i=0; i<optlen; i++){
+		const option = document.createElement("div");
+		option.innerHTML = queindex.options[i];
+		option.id = i;
+		option.style.animationDelay = animationdelay + 's'; 
+		animationdelay = animationdelay + 0.15;
+		option.className = "opt";
+		optcont.appendChild(option);
+		option.setAttribute("onclick", "getResults(this)");
+	}
+	
+}
+
+function getResults(optionElem){
+	const optid = parseInt(optionElem.id);
+	//console.log(typeof optid);
+	if(optid===availablequestions[index].correctAnswer){
+		//console.log('correct');
+		optionElem.classList.add("correct");
+		correctans++;
+			attempt++;
+	}
+	else{
+		//console.log('wrong');
+		optionElem.classList.add("wrong");
+		const opt_len = optcont.children.length;
+	for(let i=0; i<opt_len; i++){
+		if(parseInt(optcont.children[i].id)===availablequestions[index].correctAnswer){
+			optcont.children[i].classList.add("correct");
+		}
+	}
+		attempt++;
+	}
+
+	unclickableopt();
+}
+
+function unclickableopt(){
+	const opt_len = optcont.children.length;
+	for(let i=0; i<opt_len; i++){
+		optcont.children[i].classList.add("already-answered");
+	}
+}
+
+function next(){
+	if(index==9){
+		quizOver();
+	}
+	else{
+	optlength = availablequestions[index].options.length;
+	//console.log(optlength);
+	
+	for(var k=0; k<optlength; k++){
+	var elem = document.getElementById(k);
+	if(elem!=0){
+	elem.parentNode.removeChild(elem);}}
+	quesCounter++;
+	index++;
+	getNewQues(index);
+	}
+}
+function prev(){
+	optlength = availablequestions[index].options.length;
+	for(var k=0; k<optlength; k++){
+	var elem = document.getElementById(k);
+	elem.parentNode.removeChild(elem);}
+	quesCounter--;
+	index--;
+	getNewQues(index);
+	
+}
+function quizOver(){
+	quizBox.classList.add("hide");
+	resultBox.classList.remove("hide");
+	quizResult();
+}
+
+function quizResult(){
+	console.log(attempt);
+	resultBox.querySelector(".total-question").innerHTML = myQuestions.length;
+	resultBox.querySelector(".total-attempt").innerHTML = attempt;
+	resultBox.querySelector(".total-correct").innerHTML = correctans;
+	resultBox.querySelector(".total-wrong").innerHTML = attempt-correctans;
+}
+
+function resetquiz(){
+	quesCounter = 0;
+	attempt = 0;
+    correctans = 0;
+	index = 0;
+	document.location.href=" ";
+}
+
+function startquiz(){
+	homeBox.classList.add('hide');
+	quizBox.classList.remove('hide');
+	getNewQues(index);
+}
---
+++ js/question.js
@@ -0,0 +1,92 @@
+const myQuestions = [
+    {
+      question: "How is COVID-19 passed on?",
+      options: [
+        "Through droplets that come from your mouth and nose when you cough or breathe out",
+        "In sexual fluids, including semen, vaginal fluids or anal mucous",
+        "By drinking unclean water"
+      ],
+      correctAnswer:0
+    },
+    {
+      question: "What are the common symptoms of COVID-19?",
+      options: [
+        "A new and continuous cough",
+        "Fever",
+        "All of the above",
+		"None"
+      ],
+      correctAnswer:2
+    },
+    {
+      question: "Can you always tell if someone has COVID-19?",
+      options: [
+        "No – not everyone with COVID-19 has symptoms",
+        "Yes – it will be obvious, a person with COVID-19 coughs a lot",
+        "Yes – you can tell just by where a person comes from, their race and ethnicity",
+      ],
+      correctAnswer:0
+    },
+	{
+      question: "Can washing your hands protect you from COVID-19?",
+      options: [
+        "Yes – but only if you use a strong bleach",
+        "Yes – normal soap and water or hand sanitizer is enough",
+        "No – Washing your hands doesn’t stop COVID-19"
+      ],
+      correctAnswer:1
+    },
+	{
+      question: "Are people living with HIV always more at risk? ",
+      options: [
+        "Yes – people with HIV have weaker immune systems",
+        "No – people who adhere to antiretroviral treatment (ART) and have a high CD4 count aren’t more at risk"
+      ],
+      correctAnswer:1
+    },
+	{
+      question: "Which of the following people is COVID-19 more dangerous for?",
+      options: [
+        "European people",
+        "Older people – especially those aged 70 and above",
+        "Children"
+      ],
+      correctAnswer:1
+    },
+	{
+      question: "When should fabric face masks be worn?",
+      options: [
+        "In confined or crowded spaces",
+        "On public transport",
+        "All of the above"
+      ],
+      correctAnswer:2
+    },
+	{
+      question: "Can COVID-19 be cured? ",
+      options: [
+        "Yes – Hot drinks can cure COVID-19",
+        "No – but most people get better by themselves",
+        "No – COVID-19 is a death sentence"
+      ],
+      correctAnswer:1
+    },
+	{
+      question: "Which of the following is an example of physical distancing? ",
+      options: [
+        "You stop going to crowded places and visiting other people’s houses",
+        "You stop talking to the people you live with",
+        "You stop speaking to your friends on the phone"
+      ],
+      correctAnswer:0
+    },
+	{
+      question: "How can people living with HIV protect themselves from COVID-19?",
+      options: [
+        "Wash their hands regularly and follow the physical distancing advice",
+        "Exercise regularly, eat well and look after their mental health",
+        "All of the above"
+      ],
+      correctAnswer: 2
+    }
+  ];
