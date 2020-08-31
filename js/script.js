b60b5f3e8235ff99d9e597c592983e9cc4bcba68
Samiksha1512 <63280469+Samiksha1512@users.noreply.github.com>
2020-08-31 22:04:57 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,10 +15,11 @@ let totalquestions = questions.length;
 let highscorelist = document.getElementById('highscorelist');
 
 
-for(var j=1;j<=totalquestions;j++){
+for(var j=0;j<=totalquestions-1;j++){
 	var button = document.createElement("button");
 	var appdiv = document.getElementById('navbarscreen');
-	button.innerHTML = j;
+	button.id = j+1;
+	button.innerHTML = j+1;
 	appdiv.appendChild(button);	
 }
 
@@ -27,57 +28,68 @@ let getbtns = document.getElementById('navbarscreen').querySelectorAll('button')
 getbtns[k].addEventListener("click", function(){
   var btn1 = questions.findIndex(obj=>obj.question===questions[k].question);
   console.log(btn1);
+  index=btn1;
   printQuestions(btn1);
 });
 }*/
 getbtns[0].addEventListener("click", function(){
   var btn1 = questions.findIndex(obj=>obj.question===questions[0].question);
   console.log(btn1);
+  index=btn1;
   printQuestions(btn1);
 });
 getbtns[1].addEventListener("click", function(){
   var btn2 = questions.findIndex(obj=>obj.question===questions[1].question);
   console.log(btn2);
+  index=btn2;
   printQuestions(btn2);
 });
 getbtns[2].addEventListener("click", function(){
   var btn3 = questions.findIndex(obj=>obj.question===questions[2].question);
   console.log(btn3);
+  index=btn3;
   printQuestions(btn3);
 });
 getbtns[3].addEventListener("click", function(){
   var btn4 = questions.findIndex(obj=>obj.question===questions[3].question);
   console.log(btn4);
+  index=btn4;
   printQuestions(btn4);
 });
 getbtns[4].addEventListener("click", function(){
   var btn5 = questions.findIndex(obj=>obj.question===questions[4].question);
   console.log(btn5);
+  index=btn5;
   printQuestions(btn5);
 });
 getbtns[5].addEventListener("click", function(){
   var btn6 = questions.findIndex(obj=>obj.question===questions[5].question);
   console.log(btn6);
+  index=btn6;
   printQuestions(btn6);
 });
 getbtns[6].addEventListener("click", function(){
   var btn7 = questions.findIndex(obj=>obj.question===questions[6].question);
   console.log(btn7);
+  index=btn7;
   printQuestions(btn7);
 });
 getbtns[7].addEventListener("click", function(){
   var btn8 = questions.findIndex(obj=>obj.question===questions[7].question);
   console.log(btn8);
+  index=btn8;
   printQuestions(btn8);
 });
 getbtns[8].addEventListener("click", function(){
   var btn9 = questions.findIndex(obj=>obj.question===questions[8].question);
   console.log(btn9);
+  index=btn9;
   printQuestions(btn9);
 });
 getbtns[9].addEventListener("click", function(){
   var btn10 = questions.findIndex(obj=>obj.question===questions[9].question);
   console.log(btn10);
+  index=btn10;
   printQuestions(btn10);
 });
 
@@ -104,7 +116,7 @@ function start(){
 	 
 	 if(counter==totaltime){
 		alert("Times's up.<br />Press Ok to see the results");
-		 result();
+		 //result();
 		 clearInterval(timer);
 	 }
 	 //console.log("min = " + min);
@@ -117,14 +129,23 @@ function start(){
  //shownavbar();
  
  function printQuestions(i){
+	 if(getbtns[i].style.backgroundColor=='green' || getbtns[i].style.backgroundColor=='red'){
+		 //alert('already answered');
+		 document.getElementById('opt1').setAttribute('onclick','');
+	  document.getElementById('opt2').setAttribute('onclick','');
+	  document.getElementById('opt3').setAttribute('onclick','');
+	  document.getElementById('opt4').setAttribute('onclick','');
+	 }
 	 //console.log(questions[0]);
 	 document.getElementsByClassName('questionbox')[0].innerHTML=(questions.findIndex(obj=>obj.question===questions[i].question) + 1) + " " + questions[i].question;
 	 var opt=document.getElementsByClassName('optionbox');
 	 //var spans=opt.getElementsByTagName('span');
+
 	 document.getElementById('opt1').innerHTML=questions[i].options[0];
 	 document.getElementById('opt2').innerHTML=questions[i].options[1];
 	 document.getElementById('opt3').innerHTML=questions[i].options[2];
 	 document.getElementById('opt4').innerHTML=questions[i].options[3];
+	 
  }
  
  
@@ -132,7 +153,7 @@ function start(){
 	 var optionclicked = option.getAttribute("data-opt");
 	 var opts = document.getElementById('optionsbox').querySelectorAll('span');
 	 var opt = questions.findIndex(x=>x.options[0]===opts[0].innerHTML);
-	 //console.log(questions[index]);
+	  console.log(index);
 	 if(optionclicked==questions[index].answer){
 		 option.classList.add("right");
 		 getbtns[opt].style.backgroundColor='green';
@@ -155,8 +176,9 @@ function start(){
  }
  
  function shownext(){
+	 //document.getElementById('right').style.backgroundColor=document.getElementById(index+1).style.backgroundColor;
 	 if(index>=questions.length-1){
-		 if(!confirm("You have gone through all questions, Press OK to see results")){
+		 if(!confirm("You are at last question, Press OK to see results")){
 		 return;
 		 }
 		 else{
@@ -250,14 +272,4 @@ function viewhighscores(){
 	 
 	 
 }
- 
-
- 
- 
- 
- 
- 
- 
- 
- 
  
